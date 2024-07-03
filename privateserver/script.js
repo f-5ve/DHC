3089ffb1e83cc01ec00c9f388cf9629d893e63c0
Five <fivedeisgns@gmail.com>
2024-07-03 12:07:06 +0100

    Delete privateserver directory

--- privateserver/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    setTimeout(() => {
-        window.location.href = "https://dhc.lol/privateserver";
-    }, 5000); // Redirects after 5 seconds
-});
